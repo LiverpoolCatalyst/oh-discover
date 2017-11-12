fa4819b813bae518858679ad19841b83bc47d6a4
Amy Collin <amyc271@gmail.com>
2017-11-12 21:55:58 +0000

    Add lightbox to people page ready to take posters

--- js/people.js
+++ js/people.js
@@ -1,14 +1,46 @@
 var source;
 var newSource;
 
-$('.headshot').on('mouseenter', function() {
+$('.headshot').on('mouseenter', function () {
     source = $(this).attr('src');
     newSource = source.slice(0, -9) + '.jpg';
     console.log(newSource);
     $(this).attr('src', newSource);
 });
 
-$('.headshot').on('mouseleave', function() {
+$('.headshot').on('mouseleave', function () {
     console.log(source);
     $(this).attr('src', source);
 });
+
+
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
